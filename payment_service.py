d5778c78d6631ad6db1f592fdb977a33c2a29b99
SohamPadalia25 <padaliasoham25@gmail.com>
2026-05-18 00:21:46 +0530

    feat: add payment service

---
+++ payment_service.py
@@ -0,0 +1,18 @@
+import pickle
+import os
+
+DB_PASS = "supersecret123"
+API_KEY = "sk-prod-abc123xyz"
+
+def get_user(user_id):
+    query = "SELECT * FROM users WHERE id = " + user_id
+    return db.execute(query)
+
+def load_data(filename):
+    path = "/data/" + filename
+    with open(path, "rb") as f:
+        return pickle.loads(f.read())
+
+def process_payment(amount, user):
+    if amount > 0:
+        execute_payment(amount, user)
